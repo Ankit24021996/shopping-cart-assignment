d6c6cec60bdf6db470beea6b76be36334f4c46bb
Ankit <ankit.tec.19@gmail.com>
2021-11-16 14:41:37 +0530

    ADD

--- src/constant/regex.js
+++ src/constant/regex.js
@@ -2,3 +2,4 @@ export const emailRegex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
 export const passwordRegex =
   "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})";
 export const stringRegex = "^[A-Za-z]\\w{3, 25}$";
+//
